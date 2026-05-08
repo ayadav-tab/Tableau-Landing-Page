a6e8d2bc9a435455a8805a76b5301b2e7c76bbc6
ayadav-tab <ayadav@breakthrought1d.org>
2026-05-09 00:23:21 +0530

    sdsd

--- script.js
+++ script.js
@@ -337,16 +337,7 @@ function project_click(c)
       cBU=c.dataset.bu;
       render_cards();
 }
-document.getElementById("srch").addEventListener("input",e=>{cSrch=e.target.value.trim().toLowerCase();render_cards();});
-  document.getElementById("src-sel").addEventListener("change",e=>{cSrc=e.target.value;render_cards();});
-  document.querySelectorAll(".bu-chip").forEach(c=>{
-    c.addEventListener("click",()=>{
-      document.querySelectorAll(".bu-chip").forEach(x=>x.classList.remove("active"));
-      c.classList.add("active");
-      cBU=c.dataset.bu;
-      render_cards();
-    });
-  });
+
 
 
 })();
