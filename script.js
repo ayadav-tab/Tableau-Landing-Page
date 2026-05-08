b3644657d7f9f164106f0efb29ef4c1b2f631841
ayadav-tab <ayadav@breakthrought1d.org>
2026-05-08 17:51:46 +0530

    ssds

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ let bu_data;
  
 function loadSelectedSheet() {
      
-
+    console.log(tableau.extensions);
     list_worksheet = tableau.extensions.dashboardContent.dashboard.worksheets
         .find(ws => ws.name === 'list');
 
