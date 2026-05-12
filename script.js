7ce420a03d59b92ddef38a2a17ba96844fe26f72
ayadav-tab <ayadav@breakthrought1d.org>
2026-05-12 20:45:29 +0530

    Only Show BU for which Dashboards are available

--- script.js
+++ script.js
@@ -49,8 +49,15 @@
     $('#bu_count').text(bu_data.length);
 
     let html = `<div class="bu-chip active" data-bu="all">All <span class="cnt" id="cnt-all">${list_data.length}</span></div>`;
+    
+    const filteredProjects_ = new Set(
+      list_data.map(item => item.Project)
+    );
 
-    bu_data.forEach(row => {
+    const result_ = bu_data.filter(item =>
+      filteredProjects_.has(item.Project)
+    );
+    result_.forEach(row => {
       let bu_count = list_data.filter(x => x.Project === row.Project).length;
       html += `
             <div class="bu-chip"  data-bu="${row.Project}">${row.Project_Label} <span class="cnt">${bu_count}</span></div>
